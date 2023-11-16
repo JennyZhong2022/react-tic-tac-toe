95de5145f5f46049f0f12662691e468c4adf3e32
Xiao Zhong <zhongxiao2017@gmail.com>
2023-11-16 13:58:28 +1100

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import Board from './Board'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <p>haha</p>
+     <Board/>
+    </div>
+  )
+}
+
+export default App
